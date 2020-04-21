949215e1f91a9319db87cb9fea064c5f25a78ecc
juanigp <juanigpisula@gmail.com>
2020-04-21 17:43:13 -0300

    cambios en css y html

--- js/plot_time_series_js.js
+++ js/plot_time_series_js.js
@@ -1,5 +1,10 @@
 //enum definition
 var StatsEnum = {TOTAL : 0, ACTIVE : 1, DEATHS: 2, RECOVERED : 3 };
+//global variable which says which stat to plot
+var stat_to_plot = StatsEnum.TOTAL
+
+//global variable to hold the fetched data
+var data = {};
 
 //query (which country to plot)
 var query_string = decodeURIComponent(window.location.search);
@@ -16,40 +21,42 @@ var value_to_plot = function(d, stat_to_plot){
     return out
 }
 
-
 //d3 config
+//size of graph
 var margin = { top: 30, right: 120, bottom: 30, left: 50 },
-    width = 960 - margin.left - margin.right,
+    width = 900 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom,
     tooltip = { width: 100, height: 100, x: 10, y: -30 };
 
+//date bisector function
 var bisectDate = d3.bisector(function(d) { return d.last_updated; }).left,
     formatValue = d3.format(","),
     dateFormatter = d3.time.format("%d/%m/%y");
 
-var x = d3.time.scale()
+//scale to apply to x axis
+var x_axis_scale = d3.time.scale()
         .range([0, width]);
 
-var y = d3.scale.linear()
+//scale to apply to y axis
+var y_axis_scale = d3.scale.linear()
         .range([height, 0]);
 
-var xAxis = d3.svg.axis()
-    .scale(x)
+//svg x axis
+var x_axis = d3.svg.axis()
+    .scale(x_axis_scale)
     .orient("bottom")
     .tickFormat(dateFormatter);
 
-var yAxis = d3.svg.axis()
-    .scale(y)
+//svg y axis
+var y_axis = d3.svg.axis()
+    .scale(y_axis_scale)
     .orient("left")
     .tickFormat(d3.format("s"))
 
-//global variable which says which stat_to_plot
-var stat_to_plot = StatsEnum.TOTAL
-
 //line, uses stat_to_plot
 var line = d3.svg.line()
-    .x(function(d) { return x(d.last_updated); })
-    .y(function(d) { return y(value_to_plot(d, stat_to_plot)); });
+    .x(function(d) { return x_axis_scale(d.last_updated); })
+    .y(function(d) { return y_axis_scale(value_to_plot(d, stat_to_plot)); });
 
 //svg graphic
 var svg = d3.select("body").append("svg")
@@ -58,13 +65,10 @@ var svg = d3.select("body").append("svg")
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-//global variable to hold the fetched data
-var data = {};
-
 //fetch json
 var tomorrow = new Date();
 tomorrow.setDate(new Date().getDate()+1);
-date_str = tomorrow.getFullYear().toString()+"-"+(tomorrow.getMonth() + 1).toString().padStart(2,"0")+"-"+tomorrow.getDate().toString().padStart(2,"0");
+var date_str = tomorrow.getFullYear().toString()+"-"+(tomorrow.getMonth() + 1).toString().padStart(2,"0")+"-"+tomorrow.getDate().toString().padStart(2,"0");
 
 d3.json("https://api.coronatracker.com/v3/analytics/trend/country?countryCode="+country_code+"&startDate=2019-04-15&endDate="+date_str, function(error, data_) {
     //error handling
@@ -72,7 +76,7 @@ d3.json("https://api.coronatracker.com/v3/analytics/trend/country?countryCode="+
 
     data = data_;
     //name of country in html
-    document.getElementById("myText").innerHTML = data[0].country;
+    document.getElementById("country_name").innerHTML = data[0].country;
     //just change the last_updated field in the data
     data.forEach(function(d) {
         d.last_updated = new Date(d.last_updated);
@@ -83,17 +87,17 @@ d3.json("https://api.coronatracker.com/v3/analytics/trend/country?countryCode="+
         return a.last_updated - b.last_updated;
     });
 
-    x.domain([data[0].last_updated, data[data.length - 1].last_updated]);
-    y.domain(d3.extent(data, function(d) { return value_to_plot(d,stat_to_plot); }));
+    x_axis_scale.domain([data[0].last_updated, data[data.length - 1].last_updated]);
+    y_axis_scale.domain(d3.extent(data, function(d) { return value_to_plot(d,stat_to_plot); }));
 
     svg.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
+        .call(x_axis);
 
     svg.append("g")
         .attr("class", "y axis")
-        .call(yAxis)
+        .call(y_axis)
 
 
     svg.append("path")
@@ -142,12 +146,12 @@ d3.json("https://api.coronatracker.com/v3/analytics/trend/country?countryCode="+
 
     //tooltip
     function mousemove() {
-        var x0 = x.invert(d3.mouse(this)[0]),
+        var x0 = x_axis_scale.invert(d3.mouse(this)[0]),
             i = bisectDate(data, x0, 1),
             d0 = data[i - 1],
             d1 = data[i],
             d = x0 - d0.last_updated > d1.last_updated - x0 ? d1 : d0;
-        focus.attr("transform", "translate(" + x(d.last_updated) + "," + y(value_to_plot(d, stat_to_plot)) + ")");
+        focus.attr("transform", "translate(" + x_axis_scale(d.last_updated) + "," + y_axis_scale(value_to_plot(d, stat_to_plot)) + ")");
         focus.select(".tooltip-date").text(dateFormatter(d.last_updated));
         focus.select(".tooltip-total_confirmed").text(formatValue(value_to_plot(d, stat_to_plot)));
     }
@@ -159,7 +163,7 @@ function update_graphic(stat) {
     //update global variable
     stat_to_plot = stat
     //update y axis domain
-    y.domain(d3.extent(data, function(d) { return value_to_plot(d,stat_to_plot); }));
+    y_axis_scale.domain(d3.extent(data, function(d) { return value_to_plot(d,stat_to_plot); }));
     // select the section we want to apply our changes to
     var svg = d3.select("body").transition();
 
@@ -170,9 +174,9 @@ function update_graphic(stat) {
         .attr("d", line(data));
     svg.select(".x.axis") // change the x axis
         .duration(750)
-        .call(xAxis);
+        .call(x_axis);
     svg.select(".y.axis") // change the y axis
         .duration(750)
-        .call(yAxis);
+        .call(y_axis);
 
 }
