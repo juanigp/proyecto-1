43243dc0579f771e905397f3dbd8fac66ee036b2
juanigp <juanigpisula@gmail.com>
2020-04-25 19:58:21 -0300

    minor changes

--- js/app.js
+++ js/app.js
@@ -3,65 +3,67 @@
 
 
     d3.json(url, function(error, data){
-        if (error) throw error; 
+        if (error) alert("We are having trouble getting the data from the server, please try again later."); 
+        else{
         //JSON Data
-        var only_values = data.map(function(obj){ return obj.totalConfirmed; });
-        var minValue = Math.min.apply(null, only_values), maxValue = Math.max.apply(null, only_values);
-        var paletteScale = d3.scale.log().domain([0.5,maxValue]).range(["#FFFFFF","#FF6640"]); 
-            
-        data.forEach(function(item){
-            var iso = ISO2_to_ISO3(item.countryCode);
-            dataset[iso] = {total_cases : item.totalConfirmed, 
-                            active_cases : item.activeCases,
-                            total_recovered: item.totalRecovered,
-                            total_deaths : item.totalDeaths,
-                            daily_confirmed : item.dailyConfirmed,
-                            daily_deaths : item.dailyDeaths,
-                            fillColor: paletteScale(item.totalConfirmed,).toUpperCase() };
-            });
-
-        new Datamap({
-            element: document.getElementById('container1'),
-            projection: 'mercator', 
-            // countries don't listed in dataset will be painted with this color
-            fills: { defaultFill: '#888888' },
-            data: dataset,
-            geographyConfig: {
-                borderColor: '#DEDEDE',
-                highlightBorderWidth: 2,
-                // don't change color on mouse hover
-                highlightFillColor: function(geo) {
-                    return geo["fillColor"] || '#000000';
-                },
-                // only change border
-                highlightBorderColor: '#B7B7B7',
-                // show desired information in tooltip
-                popupTemplate: function(geo, data) {
-                    // don't show tooltip if country don't present in dataset
-                    if (!data) { return ; }
-                    // tooltip content
-                    return ['<div class="hoverinfo">',
-                            '<strong>', geo.properties.name, '</strong>',
-                            '<br><strong>Total cases:</strong> ', data.total_cases,
-                            '<br><strong>Active cases:</strong> ', data.active_cases,
-                            '<br><strong>Total recovered:</strong> ', data.total_recovered,
-                            '<br><strong>Total deaths:</strong> ', data.total_deaths,
-                            '<br><strong>Daily confirmed:</strong> ', data.daily_confirmed,
-                            '<br><strong>Daily deaths:</strong> ', data.daily_deaths,
-                            '</div>'].join('');
-                }
-            },
-            done: function(datamap) {
-                datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography) {
-                    var country_code = ISO3_to_ISO2(geography.id);
-                    if (!["CG","LS","KP","TJ","TM","TF","FK","SS",undefined].includes(country_code)){
-                        url = "plot_time_series_html.html?country_code="+country_code;
-                       window.location.href = url;
-                    }
+            var only_values = data.map(function(obj){ return obj.totalConfirmed; });
+            var minValue = Math.min.apply(null, only_values), maxValue = Math.max.apply(null, only_values);
+            var paletteScale = d3.scale.log().domain([0.5,maxValue]).range(["#FFFFFF","#FF6640"]); 
+                
+            data.forEach(function(item){
+                var iso = ISO2_to_ISO3(item.countryCode);
+                dataset[iso] = {total_cases : item.totalConfirmed, 
+                                active_cases : item.activeCases,
+                                total_recovered: item.totalRecovered,
+                                total_deaths : item.totalDeaths,
+                                daily_confirmed : item.dailyConfirmed,
+                                daily_deaths : item.dailyDeaths,
+                                fillColor: paletteScale(item.totalConfirmed,).toUpperCase() };
                 });
-            }
 
-        });        
+            new Datamap({
+                element: document.getElementById('container1'),
+                projection: 'mercator', 
+                // countries don't listed in dataset will be painted with this color
+                fills: { defaultFill: '#888888' },
+                data: dataset,
+                geographyConfig: {
+                    borderColor: '#DEDEDE',
+                    highlightBorderWidth: 2,
+                    // don't change color on mouse hover
+                    highlightFillColor: function(geo) {
+                        return geo["fillColor"] || '#000000';
+                    },
+                    // only change border
+                    highlightBorderColor: '#B7B7B7',
+                    // show desired information in tooltip
+                    popupTemplate: function(geo, data) {
+                        // don't show tooltip if country don't present in dataset
+                        if (!data) { return ; }
+                        // tooltip content
+                        return ['<div class="hoverinfo">',
+                                '<strong>', geo.properties.name, '</strong>',
+                                '<br><strong>Total cases:</strong> ', data.total_cases,
+                                '<br><strong>Active cases:</strong> ', data.active_cases,
+                                '<br><strong>Total recovered:</strong> ', data.total_recovered,
+                                '<br><strong>Total deaths:</strong> ', data.total_deaths,
+                                '<br><strong>Daily confirmed:</strong> ', data.daily_confirmed,
+                                '<br><strong>Daily deaths:</strong> ', data.daily_deaths,
+                                '</div>'].join('');
+                    }
+                },
+                done: function(datamap) {
+                    datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography) {
+                        var country_code = ISO3_to_ISO2(geography.id);
+                        if (!["CG","LS","KP","TJ","TM","TF","FK","SS",undefined].includes(country_code)){
+                            url = "plot_time_series_html.html?country_code="+country_code;
+                           window.location.href = url;
+                        }
+                    });
+                }
+
+            });
+        }        
    })
 
     
--- js/plot_time_series_js.js
+++ js/plot_time_series_js.js
@@ -23,15 +23,16 @@ var value_to_plot = function(d, stat_to_plot){
 
 //d3 config
 //size of graph
-var margin = { top: 30, right: 120, bottom: 30, left: 50 },
-    width = 900 - margin.left - margin.right,
+var margin = { top: 30, right: 150, bottom: 30, left: 150 },
+    width = 1000 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom,
     tooltip = { width: 100, height: 100, x: 10, y: -30 };
 
 //date bisector function
 var bisectDate = d3.bisector(function(d) { return d.last_updated; }).left,
     formatValue = d3.format(","),
-    dateFormatter = d3.time.format("%d/%m/%y");
+    dateFormatterTicks = d3.time.format("%d/%m/%y"),
+    dateFormatterTooltip = d3.time.format("%d/%m/%y %H:%M");
 
 //scale to apply to x axis
 var x_axis_scale = d3.time.scale()
@@ -44,8 +45,9 @@ var y_axis_scale = d3.scale.linear()
 //svg x axis
 var x_axis = d3.svg.axis()
     .scale(x_axis_scale)
+    .ticks(7)
     .orient("bottom")
-    .tickFormat(dateFormatter);
+    .tickFormat(dateFormatterTicks);
 
 //svg y axis
 var y_axis = d3.svg.axis()
@@ -72,87 +74,94 @@ var date_str = tomorrow.getFullYear().toString()+"-"+(tomorrow.getMonth() + 1).t
 
 d3.json("https://api.coronatracker.com/v3/analytics/trend/country?countryCode="+country_code+"&startDate=2019-04-15&endDate="+date_str, function(error, data_) {
     //error handling
-    if (error) throw error;
+    if (error) alert("We are having trouble getting the data from the server, please try again later."); 
+    else{
+        data = data_;
+        //name of country in html
+        document.getElementById("country_name").innerHTML = data[0].country;
+        //just change the last_updated field in the data
+        data.forEach(function(d) {
+            d.last_updated = new Date(d.last_updated);//.substring(0, 23));
+        });
 
-    data = data_;
-    //name of country in html
-    document.getElementById("country_name").innerHTML = data[0].country;
-    //just change the last_updated field in the data
-    data.forEach(function(d) {
-        d.last_updated = new Date(d.last_updated);
-    });
+        //svg stuff
+        
+        data.sort(function(a, b) {
+            return a.last_updated - b.last_updated;
+        });
+        
 
-    //svg stuff
-    data.sort(function(a, b) {
-        return a.last_updated - b.last_updated;
-    });
+        //update axis domain
+        x_axis_scale.domain([data[0].last_updated, data[data.length - 1].last_updated]);
+        y_axis_scale.domain(d3.extent(data, function(d) { return value_to_plot(d,stat_to_plot); }));
 
-    x_axis_scale.domain([data[0].last_updated, data[data.length - 1].last_updated]);
-    y_axis_scale.domain(d3.extent(data, function(d) { return value_to_plot(d,stat_to_plot); }));
+        //append axis
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(x_axis);
 
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(x_axis);
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(y_axis)
 
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(y_axis)
+        //line
+        svg.append("path")
+            .datum(data)
+            .attr("class", "line")
+            .attr("d", line);
+       
+        //tooltip things
+        var focus = svg.append("g")
+            .attr("class", "focus");
 
+        focus.append("circle")
+            .attr("r", 5);
+        
+        focus.append("rect")
+            .attr("class", "tooltip")
+            .attr("width", 120)
+            .attr("height", 50)
+            .attr("x", 10)
+            .attr("y", -22)
+            .attr("rx", 4)
+            .attr("ry", 4);
+        
+        focus.append("text")
+            .attr("class", "tooltip-date")
+            .attr("x", 18)
+            .attr("y", -2);
 
-    svg.append("path")
-        .datum(data)
-        .attr("class", "line")
-        .attr("d", line);
+        focus.append("text")
+            .attr("x", 18)
+            .attr("y", 18)
+            .text("Cases:");
 
-    var focus = svg.append("g")
-        .attr("class", "focus");
+        focus.append("text")
+            .attr("class", "tooltip-cases")
+            .attr("x", 65)
+            .attr("y", 18);
 
-    focus.append("circle")
-        .attr("r", 5);
-    
-    focus.append("rect")
-        .attr("class", "tooltip")
-        .attr("width", 100)
-        .attr("height", 50)
-        .attr("x", 10)
-        .attr("y", -22)
-        .attr("rx", 4)
-        .attr("ry", 4);
-    
-    focus.append("text")
-        .attr("class", "tooltip-date")
-        .attr("x", 18)
-        .attr("y", -2);
+        svg.append("rect")
+            .attr("class", "overlay")
+            .attr("width", width)
+            .attr("height", height)
+            .on("mouseover", function() { focus.style("display", null); })
+            .on("mouseout", function() { focus.style("display", "none"); })
+            .on("mousemove", mousemove);
 
-    focus.append("text")
-        .attr("x", 18)
-        .attr("y", 18)
-        .text("Cases: ");
-
-    focus.append("text")
-        .attr("class", "tooltip-cases")
-        .attr("x", 60)
-        .attr("y", 18);
-
-    svg.append("rect")
-        .attr("class", "overlay")
-        .attr("width", width)
-        .attr("height", height)
-        .on("mouseover", function() { focus.style("display", null); })
-        .on("mouseout", function() { focus.style("display", "none"); })
-        .on("mousemove", mousemove);
-
-    //tooltip
-    function mousemove() {
-        var x0 = x_axis_scale.invert(d3.mouse(this)[0]),
-            i = bisectDate(data, x0, 1),
-            d0 = data[i - 1],
-            d1 = data[i],
-            d = x0 - d0.last_updated > d1.last_updated - x0 ? d1 : d0;
-        focus.attr("transform", "translate(" + x_axis_scale(d.last_updated) + "," + y_axis_scale(value_to_plot(d, stat_to_plot)) + ")");
-        focus.select(".tooltip-date").text(dateFormatter(d.last_updated));
-        focus.select(".tooltip-cases").text(formatValue(value_to_plot(d, stat_to_plot)));
+        //tooltip
+        
+        function mousemove() {
+            var x0 = x_axis_scale.invert(d3.mouse(this)[0]),
+                i = bisectDate(data, x0, 1),
+                d0 = data[i - 1],
+                d1 = data[i],
+                d = x0 - d0.last_updated > d1.last_updated - x0 ? d1 : d0;
+            focus.attr("transform", "translate(" + x_axis_scale(d.last_updated) + "," + y_axis_scale(value_to_plot(d, stat_to_plot)) + ")");
+            focus.select(".tooltip-date").text(dateFormatterTooltip(d.last_updated));
+            focus.select(".tooltip-cases").text(formatValue(value_to_plot(d, stat_to_plot)));
+        }
     }
 });
 
